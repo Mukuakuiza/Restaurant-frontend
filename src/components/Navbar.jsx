f31bf5ab568e647aadb6f438f637e0bcd4bd50b7
Mukuakuiza <hinomukuakuiza12@gmail.com>
2025-07-13 00:24:39 +0200

    created dishes and dishcard components to show different dishes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,10 +29,10 @@ const Navbar =()=>{
 
     return(
         <nav className="fixed top-4 z-50 flex w-full flex-col items-center justify-center">
-            <div className="flex w-full items-center justify-between overflow-y-hidden p-6  lg:m-2 lg:w=[50rem] lg:rounded-full">
+            <div className="flex w-full items-center justify-between overflow-y-hidden p-6  lg:m-2 lg:w=[50rem]">
                 <img src={logo} alt="Logo" width={95} height={22} className="pr-4 cursor-pointer"/>
                 <div className="flex w-6xl">
-                    <div className="hidden uppercase space-x-6 lg:flex">
+                    <div className="hidden uppercase space-x-6 lg:flex lg:rounded-full backdrop-blur-3xl p-3 ">
                         {LINKS.map((link,idx)=>(
                             <a key={idx} href={`#${link.targetId}`} className={`text-sm ${idx !== 0 ? "border-l-2 border-neutral-300/20 pl-2" : ""} hover:opacity-50`} onClick={(e)=>handleScroll(e, link.targetId)}>
                                 {link.text}
@@ -49,7 +49,7 @@ const Navbar =()=>{
             </div>
             {
                 isMobileOpen && (
-                    <div className="w-full  lg:hidden">
+                    <div className="w-full lg:hidden  backdrop-blur-3xl">
                         {
                             LINKS.map((link, idx)=>(
                                 <a key={idx} href={`#${link.targetId}`} className="block p-4 uppercase tracking-tighter"  onClick={(e)=>handleScroll(e, link.targetId)}>
